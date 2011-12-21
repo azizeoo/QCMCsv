74b79949f6a9c452cc004afc75282a75a8f437ba
abdelaziz boussabat <a.boussabat@gmail.com>
2011-12-21 11:43:53 +0000

    COMMIT MAIN

--- src/main/java/ma/norsys/formation/csv/main/Main.java
+++ src/main/java/ma/norsys/formation/csv/main/Main.java
@@ -8,31 +8,34 @@ import java.io.File;
 import java.util.List;
 
 import ma.norsys.formation.csv.util.CSVSingleton;
-import ma.norsys.formation.dao.DaoTopicImpl;
 import ma.norsys.formation.dao.IDaoTopic;
 import ma.norsys.formation.entities.Topic;
 
-public class Main {
-	private static CSVSingleton singleton;
-	private static IDaoTopic iDaoTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
-	/**
-	 * @param args
-	 */
+@Component
+public class Main {
+
 	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"classpath:Main.xml");
+		Main p = context.getBean(Main.class);
+		p.start(args);
+	}
+
+	@Autowired
+	private IDaoTopic iDaoTopic;
+
+	private void start(String[] args) {
+		CSVSingleton singleton;
 		singleton = CSVSingleton.getInstance();
-		iDaoTopic = new DaoTopicImpl();
 		File fileQuestion = singleton.getResource(FILE_NAME_QUESTION_MAIN);
 		File fileResponse = singleton.getResource(FILE_NAME_RESPONSE_MAIN);
 		File fileTopic = singleton.getResource(FILE_NAME_TOPIC_MAIN);
-//		if (fileTopic.exists()) {
-//			List<String> linesT = singleton.readFile(fileTopic);
-			// List<Topic> listeTopic = (List<Topic>)
-			// singleton.listTopics(linesT);
-			// for (Topic topic : listeTopic) {
-			// iDaoQuestion.addTopic(topic);
-			// }
-//		}
 		// pour insérer les topics, les questions et leurs réponse
 		if (fileQuestion.exists() && fileResponse.exists()
 				&& fileTopic.exists()) {
@@ -47,5 +50,4 @@ public class Main {
 		}
 
 	}
-
 }
